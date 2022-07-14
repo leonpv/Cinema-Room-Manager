2e3afda391547cb5d39a57a8853a35e665aa8020
Sacred <plotnickovleonid@yandex.ru>
2022-07-14 17:12:31 +0300

    Four stage

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -6,13 +6,80 @@ import java.util.Scanner;
 public class Cinema {
 
     public static void main(String[] args) {
-        String[][] grid = new String[7][8];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row: ");
+        int secondNumber = scanner.nextInt();
+
+        String[][] grid = new String[firstNumber][secondNumber];
         for (String[] strings : grid) {
             Arrays.fill(strings, "S");
         }
-        //System.out.println("Cinema:");
+
+
+        int menu = 3;
+        while (menu != 0) {
+            System.out.println("1. Show the seats\n2. Buy a ticket\n0. Exit");
+            menu = scanner.nextInt();
+            switch (menu) {
+                case 1:
+                    showTheSeats(grid);
+                    break;
+                case 2:
+                    buyTicket(scanner, grid);
+                    break;
+                default:
+                    System.out.println("Choose 1, 2 or 0");
+                    break;
+            }
+        }
+
+
+        //soldStage();
+
+
+        //System.out.println("\nCinema:");
         //printBorderedGreed(grid);
-        soldStage();
+    }
+
+    private static void buyTicket(Scanner scanner, String[][] grid) {
+        System.out.println("\nEnter a row number: ");
+        int row = scanner.nextInt();
+        System.out.println("Enter a seat number in that row: ");
+        int seat = scanner.nextInt();
+
+        printTicketPrice(grid, row, seat);
+
+        grid[row - 1][seat - 1] = "B";
+    }
+
+    private static void showTheSeats(String[][] grid) {
+        System.out.println("\nCinema:");
+        printBorderedGreed(grid);
+    }
+
+    private static void printTicketPrice(String[][] grid, int row, int seat) {
+        int ticketPrice = 0;
+        int totalSeats = grid.length * (grid[grid.length - 1].length);
+        if (totalSeats < 60) {
+            ticketPrice = 10;
+        } else {
+            if (row % 2 == 1) {
+                if (row < (grid.length / 2)) {
+                    ticketPrice = 10;
+                } else {
+                    ticketPrice = 8;
+                }
+            } else {
+                if (row <= (grid.length / 2)) {
+                    ticketPrice = 10;
+                } else {
+                    ticketPrice = 8;
+                }
+            }
+        }
+        System.out.printf("%nTicket price: $%d", ticketPrice);
     }
 
     private static void soldStage() {
@@ -24,12 +91,12 @@ public class Cinema {
 
         int totalIncome = 0;
         int totalSeats = firstNumber * secondNumber;
-        if (totalSeats < 60){
+        if (totalSeats < 60) {
             totalIncome = totalSeats * 10;
         } else {
             if (firstNumber % 2 == 1) {
                 totalIncome = ((firstNumber / 2) * secondNumber * 10) +
-                              ((totalSeats - (firstNumber / 2) * secondNumber) * 8);
+                        ((totalSeats - (firstNumber / 2) * secondNumber) * 8);
             } else {
                 totalIncome = ((totalSeats / 2) * 10) + ((totalSeats / 2) * 8);
             }
